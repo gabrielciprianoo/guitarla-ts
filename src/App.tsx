bd97d9b2d9e081f396b0729647723ad6448cef62
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-25 12:15:24 -0600

    refactor: change the get data from useCart to useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useReducer } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { useCart } from './hooks/useCart'
+import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const { cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  
+  const [state, dispatch] = useReducer(cartReducer, initialState)
 
   return (
     <>
@@ -22,7 +26,7 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              {data.map((guitar) => (
+              {state.data.map((guitar) => (
                   <Guitar 
                     key={guitar.id}
                     guitar={guitar}
